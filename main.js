15cfe801f6390c7a0e2bc0f588fca0987cad5535
Bert Pareyn <pareyn.bert@gmail.com>
2012-09-01 14:24:12 +0100

    Hook up the 'admin-ui' module for a dynamic createtenant page. Combine post and get on one page /createtenant

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var adminui = require('admin-ui');
 
 var OAE = require('./util/OAE')
 var tenantUtil = require('./util/Tenant');
@@ -8,6 +9,7 @@ var tenantArr = [];
 var startOAE = function(tenantArr) {
     var server = express();
     server.listen(2000);
+    server.use(express.bodyParser());
     registerAPI(server, tenantArr);
 
     console.log('Start global server on port 2000');
@@ -20,13 +22,60 @@ var registerAPI = function(server, tennantArr) {
         res.send('Sakai OAE Global Admin Interface');
     });
 
-    server.get('/create', function(req, res, next) {
-        createNewTenant(req.query.id, req.query.name, req.query.port);
-        res.send('New tenant "' + req.query.name + '" has been fired up on port ' + req.query.port);
-    });
-
     server.get('/tenants', function(req, res, next) {
         res.send(tenantArr);
+    });
+
+    server.post('/createtenant', function(req, res) {
+        createNewTenant(req.body.id, req.body.name, req.body.port);
+        res.send('New tenant "' + req.body.name + '" has been fired up on port ' + req.body.port);
+    });
+
+    server.get('/createtenant', function(req, res, next) {
+        adminui.generateAdminUI({
+            "rows": [
+                {
+                    "element": "form",
+                    "action": "/createtenant",
+                    "method": "post",
+                    "elements": [{
+                        "element": "header",
+                        "type": "h1",
+                        "label": "Create a new tenant"
+                    }, {
+                        "element": "label",
+                        "label": "ID:",
+                        "for": "tenant_id"
+                    }, {
+                        "element": "input",
+                        "id": "tenant_id",
+                        "name": "id"
+                    }, {
+                        "element": "label",
+                        "label": "Name:",
+                        "for": "tenant_name"
+                    }, {
+                        "element": "input",
+                        "id": "tenant_name",
+                        "name": "name"
+                    }, {
+                        "element": "label",
+                        "label": "Port:",
+                        "for": "tenant_port"
+                    }, {
+                        "element": "input",
+                        "id": "tenant_port",
+                        "name": "port"
+                    }, {
+                        "element": "button",
+                        "type": "submit",
+                        "label": "Create tenant"
+                    }]
+                }
+            ]
+        }, function(html) {
+            res.send(html);
+        })
     });
 
     OAE.initializeKeySpace(function() {

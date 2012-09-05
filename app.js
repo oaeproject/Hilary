a26db71b657fb10e7a4a4692cd47fa114ffb994d
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2012-09-05 22:53:19 +0100

    Switching to Cassandra.js + Hooking up tenant end points to APIs

--- app.js
+++ app.js
@@ -1,59 +1,87 @@
 var express = require('express');
 
-var IO = require('oae-util/lib/IO');
-var OAE = require('oae-util/lib/OAE');
-var tenantUtil = require('oae-util/lib/Tenant');
+var IO = require('oae-util/lib/io');
+var OAE = require('oae-util/lib/oae');
+var Cassandra = require('oae-util/lib/cassandra');
 
-var tenantArr = [];
+var tenantAPI = require('oae-tenants');
 
-var startOAE = function(tenantArr) {
+/**
+ * Start OAE and run the global administrative interface
+ * on port 2000 by default
+ */
+var startOAE = function() {
     var server = express();
     server.listen(2000);
-    registerAPI(server, tenantArr);
-
-    console.log('Start global server on port 2000');
-    
+    registerAPI(server);
+    console.log('Starting Sakai OAE');
 };
 
-var registerAPI = function(server, tennantArr) {
+/**
+ * Register all necessary REST end points for the global admin interface
+ * @param  {Server}    server       Express object representing the global admin interface
+ */
+var registerAPI = function(server) {
     
     server.get('/whoami', function(req, res, next) {
         res.send('Sakai OAE Global Admin Interface');
     });
 
     server.get('/create', function(req, res, next) {
-        createNewTenant(req.query.id, req.query.name, req.query.port);
-        res.send('New tenant "' + req.query.name + '" has been fired up on port ' + req.query.port);
+        tenantAPI.createTenant(req.query.id, req.query.name, req.query.description, req.query.port, function(err) {
+            if (err) {
+                return res.send(500, err);
+            }
+            res.send('New tenant "' + req.query.name + '" has been fired up on port ' + req.query.port);
+        });
     });
 
     server.get('/tenants', function(req, res, next) {
-        res.send(tenantArr);
+        tenantAPI.getAllTenants(function(tenants, err) {
+            if (err) {
+                return res.send(500, err);
+            }
+            res.send(tenants);
+        });
     });
 
-    OAE.initializeKeySpace(function() {
+    Cassandra.createKeyspace('oae', function() {
         OAE.getAvailableModules(function(modules) {
-            console.log(modules);
             for (var m = 0; m < modules.length; m++) {
                 runModuleStore(modules[m]);
             }
+            startTenants();
         });
     });
 
-    var runModuleStore = function(module) {
-        var path = "node_modules/" + module + "/install/install.js";
-        IO.pathExists(path, function(exists) {
-            if (exists) {
-                require("./" + path);
-            }
-        });
-    };
-
 };
 
-var createNewTenant = function(id, name, port) {
-    var tenant = new tenantUtil.Tenant(id, name, port);
-    tenantArr.push(tenant);
-    tenantUtil.startTenant(tenant);
+/**
+ * Start up all of the registered tenants
+ */
+var startTenants = function() {
+    tenantAPI.getAllTenants(function(tenants, err) {
+        if (err) {
+            throw err;
+        }
+        for (var t = 0; t < tenants.length; t++) {
+            tenantAPI.startTenant(tenants[t]);
+        }
+    });
 };
 
-startOAE([]);
+/**
+ * For each of the installed OAE-related modules, we check if there is a Cassandra schema that
+ * needs to be initialized
+ * @param  {String}    module       Id of the module for which the schema needs to be initialized
+ */
+var runModuleStore = function(module) {
+    var path = "node_modules/" + module + "/install/install.js";
+    IO.pathExists(path, function(exists) {
+        if (exists) {
+            require("./" + path);
+        }
+    });
+};
+
+startOAE();

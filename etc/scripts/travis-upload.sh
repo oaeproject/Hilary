ba35f4b0c564a58c6dc734a116830eebeb34ca49
Branden Visser <mrvisser@gmail.com>
2014-02-19 13:29:33 -0500

    Fix issues with travis-upload.sh

--- etc/scripts/travis-upload.sh
+++ etc/scripts/travis-upload.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ##
 # This script will package and upload a successful build to Amazon S3. It should only
@@ -21,5 +21,5 @@ function package_and_upload {
 }
 
 if [[ "$TRAVIS_REPO_SLUG" == "$EXPECTED_REPOSITORY" && "$TRAVIS_BRANCH" == "$EXPECTED_BRANCH" && "$TRAVIS_PULL_REQUEST" == "$EXPECTED_PULL_REQUEST" ]]; then
-    package_and_upload()
+    package_and_upload
 fi

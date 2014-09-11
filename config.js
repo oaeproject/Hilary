d150fa8871ce596bfb1d9bcd6cddae8ddf87b843
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-09-11 16:53:02 +0100

    993 - Email grace period
    Introduces a grace period for emails.
    When an email activity stream contains an activity that was delivered in
    the last `config.activity.mail.gracePeriod` seconds, we will not deliver
    email for that stream and postpone it till the next collection.

--- config.js
+++ config.js
@@ -303,6 +303,7 @@ config.signing = {
  * @param  {Number}     [collectionBatchSize]           The number of items to process at a time when collecting bucketed activities. After one batch has been collected, the activity processor will immediately continue to process the next batch from that bucket, and so on. Defaults to 1000
  * @param  {Object}     [mail]                          Configuration for aggregated emails
  * @param  {Number}     [mail.pollingFrequency]         How often (in seconds) the email processing buckets are polled for new activities. This frequency will roughly determine the delay between an activity and sending an email for a user who has selected `immediate` and is involved in the activity. It should always be less than an hour
+ * @param  {Number}     [mail.gracePeriod]              The minimum amount of time (in seconds) that should pass before the email process can send out an e-mail for an activity. This is to allow further activities to aggregate with the activity that triggered the email. Defaults to 1 minute
  * @param  {Object}     [mail.daily]                    Configuration for the daily email aggregate collection cycle
  * @param  {Number}     [mail.daily.hour]               At what hour during the day email should be collected for daily aggregates
  * @param  {Object}     [mail.weekly]                   Configuration for the weekly email aggregate collection cycle
@@ -327,6 +328,7 @@ config.activity = {
     'collectionBatchSize': 1000,
     'mail': {
         'pollingFrequency': 10 * 60,        // 10 minutes
+        'gracePeriod':  60,                 // 1 minute
         'daily': {
             'hour': 8                       // 8AM
         },
